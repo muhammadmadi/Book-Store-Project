557bca19a3da22216bb8ff1996a2f7c35d2d3f40
muhammadmadi <muhammadmadi1990@gmail.com>
2023-09-19 01:38:33 +0300

    CORS policy in Node js and Express js

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,31 @@ import mongoose from "mongoose";
 import { Book } from "./models/bookmodel.js";
 import booksRoute from "./routes/booksRoute.js";
 import router from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 //Middlewere for parsing request body
 app.use(express.json());
+
+//Middlewere for use CORS  policcy
+//option 1 : allow all origins with default or cors(*)
+app.use(cors);
+
+//option 2 : allow custom origins
+app.use(
+  cors({
+    origin: "http://localhost:5555",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-tupe"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("velcome to bookstore tutorials");
 });
 
-app.use('/books',router);
-
+app.use("/books", router);
 
 mongoose
   .connect(mongoDBURL)
